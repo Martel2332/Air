64223a9e4d8ab9ec313272f31caead6a44d32d42
Sébastien Le Breton <74906434+SLB6666@users.noreply.github.com>
2021-03-22 11:48:28 +0100

    Update Main_Code.R

--- Main_Code.R
+++ Main_Code.R
@@ -1,9 +1,9 @@
-Air = function(niter=10^5, init=c(0,0,0,0,0), prop.sd=c(0.4,0.015)){   #ajuster prop.sd selon acc.rates
+Air = function(niter=10^5, init=c(12,27,43,-0.5,0.03), prop.sd=c(0.4,0.015)){   #ajuster prop.sd selon acc.rates
   
   alpha = 4.48        
   beta = 0.76         
   sigma2 = 81.14
-  sigma2_theta = 1024
+  sigma2_theta = 1000
   J = 3               
   y = c(21, 20, 15)
   n = c(48, 34, 21)
@@ -54,14 +54,23 @@ Air = function(niter=10^5, init=c(0,0,0,0,0), prop.sd=c(0.4,0.015)){   #ajuster 
   return(list(chain=chain, acc.rates=acc.rates/niter))
 }
 
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Etude
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 acc.rates = Air()$acc.rates
 chain = Air()$chain
 
 par(mfrow=c(1,5))
+para = c("X[1]","X[2]","X[3]","theta[1]","theta[2]")
 for (i in 1:5){
-  plot(chain[,i], type="l")
+  plot(chain[,i], type="l", xlab="Iterations", ylab=para[i])
 }
 
+mean(chain[,1])
+mean(chain[,2])
+mean(chain[,3])
 mean(chain[,4])
 mean(chain[,5])
 
@@ -73,7 +82,55 @@ summary(chain[,5])
 
 par(mfrow=c(1,5))
 for (i in 1:5){
-  acf(chain[,i], lag.max=100)
+  acf(chain[,i], lag.max=100, ylab=para[i])
 }
 
-#A faire : Elagage des theta et peut-être burning de x3
+#Trop de corrélations pour les theta -----> élagage 
+chain_elag = chain[seq(1, nrow(chain), by = 50),]
+
+par(mfrow=c(2,5))
+for (i in 1:5){
+  plot(chain_elag[,i], type="l", xlab="Iterations", ylab=para[i])
+  acf(chain_elag[,i], lag.max=1000, ylab=para[i])
+}
+
+mean(chain_elag[,1])
+mean(chain_elag[,2])
+mean(chain_elag[,3])
+mean(chain_elag[,4])
+mean(chain_elag[,5])
+
+#Confrontation numérique vs théorie
+
+alpha = 4.48        
+beta = 0.76         
+sigma2 = 81.14
+sigma2_theta = 1000
+J = 3               
+y = c(21, 20, 15)
+n = c(48, 34, 21)
+z = c(10, 30, 50)
+
+for (i in 1:3){
+  hist(chain_elag[,i], freq=FALSE, main="", xlab=para[i])
+  lines(density(rnorm(-20:80, alpha+beta*z[i], sqrt(sigma2))), lwd=2, col="blue")
+}
+
+
+
+
+
+for (i in 4:5){
+  hist(chain_elag[,i], freq=FALSE, main="", xlab=para[i])
+  lines(exp(sum(y*(theta1+theta2*rnorm(-20:80, alpha+beta*z[i], sqrt(sigma2))) 
+               - n*log(1+exp(theta1+theta2*rnorm(-20:80, alpha+beta*z[i], sqrt(sigma2)))) 
+               + (-(theta1^2+theta2^2)/(2*sigma2_theta)))), lwd=2, col="blue")
+}
+
+
+
+theta1 = theta2 = rnorm(100, 0, sqrt(sigma2_theta))
+
+exp(sum(y*(theta1+theta2*rnorm(100, alpha+beta*z[1], sqrt(sigma2))) 
+        - n*log(1+exp(theta1+theta2*rnorm(-20:80, alpha+beta*z[1], sqrt(sigma2)))) 
+        + (-(theta1^2+theta2^2)/(2*sigma2_theta))))
